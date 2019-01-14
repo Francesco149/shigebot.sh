a1568122ba03950b02864c58157829d5eec79f40
Franc[e]sco <lolisamurai@tfwno.gf>
2019-01-14 20:11:42 +0100

    log messages to log.txt

--- handlers/log.sh
+++ handlers/log.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 while read -r line; do
-  echo "$line" | sed -n s/PING/PONG/p
-  >&2 echo "[$(date '+%F %T')] $line"
+  irccmd="$(echo "$line" | awk '{ print $2 }')"
+  case "$irccmd" in
+  PING) echo "$line" | sed s/PING/PONG/ ;;
+  PRIVMSG) #:user!user@user.tmi.twitch.tv PRIVMSG #channel :text\r\n
+    username=$(echo "$line" | awk -F '[:!]' '{ printf "%s: ", $2 }')
+    [ "$user: " = "$username" ] && continue
+    msg=$(echo "$line" | cut -d ':' -f3-)
+    case "$msg" in
+      !*) continue ;;
+      *) ( printf "$username" && echo "$msg" ) | tr -d '\r' >>logs.txt ;;
+    esac
+    ;;
+  esac
 done
