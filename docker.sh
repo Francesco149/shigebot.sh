18f31689c0e4328ff6178b3c838e074c3478ec56
Franc[e]sco <lolisamurai@tfwno.gf>
2019-02-02 19:14:37 +0100

    add docker script

---
+++ docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+wdir=$(realpath "$dir")
+olddir="$(pwd)"
+cd "$wdir" || exit
+mkdir logs.mrkdb.d >/dev/null 2>&1
+mkdir tweet_cache >/dev/null 2>&1
+docker build -t shigebot . &&
+  docker run --rm \
+    --volume "$wdir/logs.txt":/logs.txt \
+    --volume "$wdir/logs99.time":/logs99.time \
+    --volume "$wdir/logs.mrkdb.d":/logs.mrkdb.d \
+    --volume "$wdir/tweet_cache":/tweet_cache \
+    "$@" shigebot
+cd "$olddir" || exit
